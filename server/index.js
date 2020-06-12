37de6599c1db4b9eb0c94b54fb4abcbcc2399aba
banusoppan <kjksopan@gmail.com>
2020-06-12 12:48:10 +0530

    first commit

---
+++ server/index.js
@@ -0,0 +1,11 @@
+const express = require('express');
+const app = express();
+app.get('/',(req,res)=>{
+    res.send({
+        'message' : "Hi there"
+    })
+
+});
+const PORT = process.env.PORT || 3000;
+
+app.listen(PORT);
