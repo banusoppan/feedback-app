39ed51d4c2176dfcbf6e7c5242bd8e467dec122c
banusoppan <kjksopan@gmail.com>
2020-07-01 00:17:13 +0530

    update gitignore

---
+++ server/config/dev.js
@@ -0,0 +1,10 @@
+module.exports = {
+    googleClientID : "868947880119-oh1mshjg2rghe48fnots1ahr18nc4qm4.apps.googleusercontent.com",
+    googleClientSecret : "TuFbhOMLAYxHO0EgWwBAQTeR",
+    cookieKey : "emaily-feedback-app-1994",
+    stripePublishableKey : "pk_test_51Gux8WJIjz44tPmMFlYAxytsmHc9kQp0MtOaoRjiyiCdmgBjkmCBqrwCsjUI9FxZPLoyRJCJyLhicuz9wajZPk2qo00IONeWzic",
+    stripeSecretKey : "sk_test_51Gux8WJIjz44tPmMhUvmjpyJ759Y0viLq1HvOdj0mmy3dho0fkot1K84dLmqXJz4S2FhoT0MsS5eqqMlW49eNNlM00faIfQFqo",
+    sendGrid : "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",
+    redirectDomain : "http://localhost:3000"
+
+}
